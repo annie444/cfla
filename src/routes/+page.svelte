93dd6325cbc75cfb62b2fdadf502d739e3df5fc0
Annie Ehler <annie.ehler.4@gmail.com>
2023-12-13 22:14:03 -0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,168 @@
+<main>
+	<div class="w-full min-h-full flex flex-col md:flex-row intro bg-green-950">
+		<div class="md:w-[40%] px-[45px] py-[50px]">
+			<div class="justify-between flex">
+				<div class="backIcon">
+					<a href="#home" class="text-white hover:text-slate-400"
+						><i class="fill-slate-400 fa-solid fa-arrow-left"></i></a
+					>
+				</div>
+				<div class="menu">
+					<ul class="flex list">
+						<li class="list-none px-[20px]">
+							<a
+								href="#our-purpose"
+								class="text-white hover:text-slate-400 no-underline font-sans text-[22px] a font-semibold relative before md:before:top-[35px] before:top-[55px]"
+								>Our Purpose</a
+							>
+						</li>
+						<li class="list-none px-[20px]">
+							<a
+								href="#our-demands"
+								class="text-white hover:text-slate-400 no-underline font-sans text-[22px] a font-semibold relative before md:before:top-[35px] before:top-[55px]"
+								>Our Demands</a
+							>
+						</li>
+						<li class="list-none px-[20px]">
+							<a
+								href="#cta"
+								class=" text-white hover:text-slate-400 no-underline font-sans text-[22px] a font-semibold relative before before:top-[35px]"
+								>Join</a
+							>
+						</li>
+						<li class="list-none px-[20px]">
+							<a href="https://instagram.com/ceasefire.la"
+								><i class="fill-blue-400 fa-brands fa-instagram"></i></a
+							>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div
+				id="home"
+				class="relative md:top-1/2 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 font-sans px-0 py-[50px]"
+			>
+				<h1 class="h1 text-[48px]/[48px]">Unnamed, United, and Unyielding</h1>
+				<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+					In the heart of Los Angeles, a coalition of Palestinians, allies, and anti-Zionist Jews
+					made up of individuals from all walks of life & organizational affiliations have emerged
+					to rally as a collective people - irrespective of our movement ties - for a critical
+					cause: <strong>Palestinian safety and liberation.</strong>
+				</p>
+				<a
+					href="#our-purpose"
+					class="text-white hover:text-slate-400 text-[23px]/[23px] font-semibold no-underline relative before md:before:top-[35px] before:top-[45px]"
+					>Read</a
+				>
+			</div>
+		</div>
+		<div class="w-full h-[100vw] md:h-full md:w-[60%]">
+			<img
+				class="w-full h-full first-img"
+				src="/1.jpeg"
+				alt="Shut it down! People of LA say ceasefire now! Friday December 15th at 3PM in Soutwest LA, location to be announced the morning of"
+			/>
+		</div>
+	</div>
+	<div
+		class="w-full min-h-full flex flex-col md:flex-row relative justify-center content-center bg-black"
+	>
+		<div class="px-[45px] py-[65px] md:w-3/4" id="our-purpose">
+			<h1 class="h1 text-[48px]/[48px]">Why We Stand Together</h1>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				We are steadfast and united as a collective people here in Los Angeles, far from the
+				immediate struggles of the Palestinian people, yet deeply connected to their plight. Our
+				purpose is to render the support systems - both economic and political - that facilitate the
+				siege on Gaza and the continued occupation of Palestine, unsustainable for all stakeholders
+				involved. We believe in the power of collective action and the necessity of global
+				solidarity to bring about meaningful change.
+			</p>
+		</div>
+	</div>
+	<div
+		class="w-full min-h-full flex flex-col md:flex-row relative justify-center content-center bg-red-950"
+	>
+		<div class="px-[45px] py-[65px] md:w-3/4" id="our-demands">
+			<h1 class="h1 text-[48px]/[48px]">Clear, Concise, Non-Negotiable</h1>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				Our collective action is driven by a set of uncompromising demands that we believe are
+				fundamental to achieving justice and peace for the Palestinian people:
+			</p>
+			<h2 class="h2 text-[40px]/[40px]">
+				Guiding Principles: <i>For Justice, For Peace, For Humanity</i>
+			</h2>
+			<br />
+			<h3 class="h3 text-[35px]/[35px]">Immediate Ceasefire</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				An end to all hostilities and violence, with a focus on protecting civilian lives and
+				infrastructure.
+			</p>
+			<h3 class="h3 text-[35px]/[35px]">End the Siege on Gaza</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				Lifting of all blockades and restrictions to ensure free movement of people, goods, and
+				services.
+			</p>
+			<h3 class="h3 text-[35px]/[35px]">Stop the Annexation of the West Bank and East Jerusalem</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				Recognition of Palestinian rights and territories in accordance with international law.
+			</p>
+			<h3 class="h3 text-[35px]/[35px]">End the Israeli Occupation</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				A call for the withdrawal of Israeli forces and an end to the occupation in all its forms.
+			</p>
+			<h3 class="h3 text-[35px]/[35px]">No Genocide in Palestine on Our Dime</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				A rejection of any actions or policies that contribute to the systematic oppression of the
+				Palestinian people.
+			</p>
+			<h3 class="h3 text-[35px]/[35px]"><i>'Never Again'</i> Means Never Again for Anyone</h3>
+			<p class="text-white text-[28px]/[28px] py-[30px] px-0">
+				A universal call for the prevention of genocide and mass atrocities, irrespective of race,
+				religion, or nationality.
+			</p>
+		</div>
+	</div>
+	<div
+		class="w-full min-h-full flex flex-col md:flex-row relative justify-center content-center bg-white"
+	>
+		<div class="px-[45px] py-[65px] md:w-3/4" id="cta">
+			<p class="text-[28px]/[28px] py-[30px] px-0 text-black">
+				We stand together in Los Angeles, committed to raising awareness, mobilizing support, and
+				taking action to ensure that the plight of the Palestinian people is not forgotten. Our
+				mission is rooted in the principles of justice, peace, and the universal rights of all
+				humans. We call upon all who share our values to join us in this critical endeavor, for a
+				world where 'never again' truly means never again for anyone.
+			</p>
+			<a
+				href="https://forms.gle/bhwJjKNqnH4x1RC78"
+				class="text-black hover:text-slate-600 no-underline font-sans text-[22px] a font-semibold relative before md:before:top-[35px] before:top-[55px]"
+				>Join us</a
+			>
+		</div>
+	</div>
+</main>
+
+<style>
+	.a {
+		transition: all 0.3s ease;
+	}
+	.before::before {
+		content: '';
+		position: absolute;
+		width: 40%;
+		height: 2px;
+		background: #c9c9c9;
+		transition: all 0.3s ease;
+		/*   display:none; */
+	}
+	.before:hover::before {
+		width: 100%;
+		background: #282828;
+	}
+	.first-img {
+		background-image: url('/1.jpeg');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
